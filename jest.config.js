131e92295475bceefc03c293420725259b47240e
Jack <ajackofsometrades@gmail.com>
2018-12-30 10:34:49 -0800

    cleaning up config files

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': '<rootDir>/src/testing/jest-preprocess.js'
+    '^.+\\.jsx?$': '<rootDir>/config/testing/jest-preprocess.js'
   },
   snapshotSerializers: [
     'enzyme-to-json/serializer'
   ],
-  setupTestFrameworkScriptFile: '<rootDir>/src/testing/jest.setup.js',
+  setupTestFrameworkScriptFile: '<rootDir>/config/testing/setupTests.js',
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
@@ -16,5 +16,5 @@ module.exports = {
     '__PATH_PREFIX__': ''
   },
   testURL: 'http://localhost',
-  setupFiles: ['<rootDir>/src/testing/loadershim.js']
+  setupFiles: ['<rootDir>/config/testing/loadershim.js']
 };
