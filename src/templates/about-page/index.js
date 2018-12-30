1dd3b044d1f971dc2bae0743cd35a25ed5446e7a
Jack <ajackofsometrades@gmail.com>
2018-12-30 06:02:59 -0800

    extracted some components

--- src/templates/about-page/index.js
+++ src/templates/about-page/index.js
@@ -2,34 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../../layout';
-import Content, { HTMLContent } from '../../components/Content';
+import { HTMLContent } from '../../components/Content';
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
-
-  return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func,
-};
+import AboutPageTemplate from './Template';
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data;
