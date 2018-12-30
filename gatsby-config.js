214f396328976425da71a88220fdb412768c4160
Jack <ajackofsometrades@gmail.com>
2018-12-30 08:52:29 -0800

    better naming

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        modulePath: `${__dirname}/src/cms/index.js`,
       },
     },
     'gatsby-plugin-purgecss', // must be after other CSS plugins
