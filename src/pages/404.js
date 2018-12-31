a4b421890e9ffa4b7d289f634f7d5073725adf28
Jack <ajackofsometrades@gmail.com>
2018-12-31 04:53:22 -0800

    I stillllll haven't found, what I'm lookin for

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import App from '../app';
+import { withPrefix } from 'gatsby';
+
+const default404 = withPrefix('/img/confused.gif');
 
 const NotFoundPage = () => (
   <App>
     <div>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <img src={default404}/>
+      <p>You just hit a route that doesn&#39;t exist...</p>
     </div>
   </App>
 );
